9e4b3e9d08d1f368c213a290207ce5a0b704af13
Taras_Spashchenko <Taras_Spashchenko@epam.com>
2024-05-16 19:04:36 +0200

    Update project dependencies and implement TLS configuration
    This commit includes the removal of the EnrichHeadersClient class and updates the `edge-common-spring` dependency version. The Spring Boot Main class has been modified to add BouncyCastleFipsProvider to Security. Configurations for enabling Transport Layer Security (TLS) for HTTP endpoints and Feign HTTP clients have been added. The README has been updated with instructions related to the new TLS configurations.

--- src/main/java/org/folio/ed/support/FeignConfig.java
+++ src/main/java/org/folio/ed/support/FeignConfig.java
@@ -1,20 +1,14 @@
 package org.folio.ed.support;
 
-import org.folio.ed.client.EnrichHeadersClient;
 import org.folio.ed.client.logger.SensitiveDataProtectionLogger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import feign.Client;
 import feign.Logger;
 
 @Configuration
 public class FeignConfig {
 
-  @Bean
-  public Client enrichHeadersClient() {
-    return new EnrichHeadersClient();
-  }
 
   @Bean
   public Logger feignLogger() {
