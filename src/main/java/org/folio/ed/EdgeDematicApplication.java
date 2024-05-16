f1f81d747890efc5d09511071e8193028eff7460
Taras_Spashchenko <Taras_Spashchenko@epam.com>
2024-05-16 19:04:36 +0200

    Update project dependencies and implement TLS configuration
    This commit includes the removal of the EnrichHeadersClient class and updates the `edge-common-spring` dependency version. The Spring Boot Main class has been modified to add BouncyCastleFipsProvider to Security. Configurations for enabling Transport Layer Security (TLS) for HTTP endpoints and Feign HTTP clients have been added. The README has been updated with instructions related to the new TLS configurations.

--- src/main/java/org/folio/ed/EdgeDematicApplication.java
+++ src/main/java/org/folio/ed/EdgeDematicApplication.java
@@ -1,6 +1,7 @@
 package org.folio.ed;
 
 
+import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,8 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.security.Security;
+
 @SpringBootApplication
 @EnableAsync
 @EnableFeignClients
@@ -19,6 +22,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
     HibernateJpaAutoConfiguration.class })
 public class EdgeDematicApplication {
   public static void main(String[] args) {
+    Security.addProvider(new BouncyCastleFipsProvider());
     SpringApplication.run(EdgeDematicApplication.class, args);
   }
 }
